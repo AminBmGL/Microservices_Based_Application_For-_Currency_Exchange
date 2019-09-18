0b3b9f6cc47474fe995119667212595503050357
Amin Bm <medaminbenmbarek@gmail.com>
2019-09-18 13:28:58 +0100

    Clean up exchage service and generationg new project for the conversion service

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10001,'USD','TND',2,86);
+values(10001,'USD','TND',2.86,0); 
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10002,'EUR','TND',3,15);
+values(10002,'EUR','TND',3.15,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'AUD','TND',1,95);
+values(10003,'AUD','TND',1.95,0);
