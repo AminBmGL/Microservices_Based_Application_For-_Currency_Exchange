89631cdb25631b6258dbb3bfb5462cfa31ab7ce9
Amin Bm <medaminbenmbarek@gmail.com>
2019-09-19 13:22:13 +0100

    Implementing Zuul Logging Filter

---
+++ netflix-zuul-api-gateway-server/src/main/java/tn/insat/netflixzuulapigatewayserver/config/ZuulLoggingFilter.java
@@ -0,0 +1,45 @@
+package tn.insat.netflixzuulapigatewayserver.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	//Logic to decide we execute filter or not 
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		HttpServletRequest request = 
+				RequestContext.getCurrentContext().getRequest();
+		logger.info("request -> {} request uri -> {}", 
+				request, request.getRequestURI());
+		return null;
+	}
+
+	
+	//To decide when executing the filtering(before the request , after it , only if error happens ...)
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
