a51d9d8991cf91821b9e29cf878b5e65069696d5
Amin Bm <medaminbenmbarek@gmail.com>
2019-09-19 16:23:09 +0100

    Adding falt Tolerance with Hystrix

--- limits-service/src/main/java/tn/insat/limitsservice/controllers/LimitsConfigurationController.java
+++ limits-service/src/main/java/tn/insat/limitsservice/controllers/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import tn.insat.limitsservice.domain.Configuration;
 import tn.insat.limitsservice.domain.LimitConfiguration;
 
@@ -14,8 +16,13 @@ public class LimitsConfigurationController {
 	private Configuration configuration;
 
 	@GetMapping("/limits")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMinimum(),
 				configuration.getMaximum());
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
 	}
 }
